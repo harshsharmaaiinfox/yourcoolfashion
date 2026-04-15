6c0c736583ed1fe00d1041f03d9e6322f0f23b8b
harshsharmaaiinfox · <harsh.sharma@aiinfox.com>
2026-04-15 12:05:57 +0530

    Removed "UPI Payment (NSDL)" from the checkout

--- src/app/shared/state/setting.state.ts
+++ src/app/shared/state/setting.state.ts
@@ -81,7 +81,7 @@ export class SettingState {
 
               {
                 name: 'insider_paywize',
-                status: true,
+                status: false,
                 title: 'UPI Payment (NSDL)',
               },
               {
